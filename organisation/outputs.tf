9434bcf90fce78fb6d14ab7bfd42c1e3d4bc9652
Doug <rootprivileges@gmail.com>
2019-08-23 10:37:51 +0100

    Add master organisation outputs

---
+++ organisation/outputs.tf
@@ -0,0 +1,15 @@
+output "master_account_id" {
+  value = "${data.aws_caller_identity.current.account_id}"
+}
+
+output "master_billing_role_policy_arn" {
+  value = "${module.assume_role_policy_master_billing.policy_arn}"
+}
+
+output "terragrunt_admin_role_policy_arn" {
+  value = "${module.assume_role_policy_terragrunt_admin.policy_arn}"
+}
+
+output "terragrunt_reader_role_policy_arn" {
+  value = "${module.assume_role_policy_terragrunt_reader.policy_arn}"
+}
