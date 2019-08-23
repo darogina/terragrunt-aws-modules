5ebf13210fed18b0282ce79eb08681b9e0148a7c
Doug <rootprivileges@gmail.com>
2019-08-23 17:37:45 +0100

    Create finance group

---
+++ iam/groups/finance/finance.tf
@@ -0,0 +1,18 @@
+data "terraform_remote_state" "master" {
+  backend = "s3"
+
+  config = {
+    bucket = "${var.tfstate_global_bucket}"
+    key    = "organisation/terraform.tfstate"
+    region = "${var.tfstate_global_bucket_region}"
+  }
+}
+
+resource "aws_iam_group" "finance" {
+  name = "finance"
+}
+
+resource "aws_iam_group_policy_attachment" "billing" {
+  group      = "${aws_iam_group.finance.name}"
+  policy_arn = "arn:aws:iam::aws:policy/job-function/Billing"
+}
