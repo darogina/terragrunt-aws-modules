d6939682699dd750d9788dd761ca8f490b4364ae
Doug <rootprivileges@gmail.com>
2019-08-23 17:16:56 +0100

    Create Administrator role for organisation

--- organisation/organisation.tf
+++ organisation/organisation.tf
@@ -5,3 +5,19 @@ resource "aws_organizations_organization" "master" {
     prevent_destroy = true
   }
 }
+
+module "cross_account_role_organisation_admin" {
+  source = "../utility/cross-account-role"
+
+  assume_role_policy_json = "${data.aws_iam_policy_document.crossaccount_assume_from_master.json}"
+  role                    = "Administrator"
+  role_policy_arn         = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+module "assume_role_policy_organisation_admin" {
+  source = "../utility/assume-role-policy"
+
+  account_name = "master"
+  account_id   = "${data.aws_caller_identity.current.account_id}"
+  role         = "Administrator"
+}
--- organisation/outputs.tf
+++ organisation/outputs.tf
@@ -1,3 +1,7 @@
+output "admin_role_policy_arn" {
+  value = "${module.assume_role_policy_organisation_admin.policy_arn}"
+}
+
 output "master_account_id" {
   value = "${data.aws_caller_identity.current.account_id}"
 }
