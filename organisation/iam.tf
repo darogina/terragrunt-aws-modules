b7cebe3901ab3cb0a41ca6fdada39537efc0c5e7
Doug <rootprivileges@gmail.com>
2019-10-23 18:09:53 +0100

    Use iam_baseline from nozaq/terraform-aws-secure-baseline

--- organisation/iam.tf
+++ organisation/iam.tf
@@ -1,9 +1,13 @@
-resource "aws_iam_account_password_policy" "strict" {
-  minimum_password_length        = 10
-  require_lowercase_characters   = true
-  require_numbers                = true
-  require_uppercase_characters   = true
-  require_symbols                = true
-  allow_users_to_change_password = true
-  max_password_age               = 90
+module "iam_baseline" {
+  source = "git::git@github.com:nozaq/terraform-aws-secure-baseline.git//modules/iam-baseline?ref=0.16.1"
+
+  aws_account_id                 = data.aws_caller_identity.current.account_id
+  manager_iam_role_name          = "MasterIamManagerRole"
+  manager_iam_role_policy_name   = "IamManager"
+  master_iam_role_name           = "MasterIamMasterRole"
+  master_iam_role_policy_name    = "IamMaster"
+  support_iam_role_name          = "MasterIamSupportRole"
+  support_iam_role_policy_name   = "IamSupport"
+  support_iam_role_principal_arn = aws_iam_user.support_user.arn
+  tags                           = {}
 }
