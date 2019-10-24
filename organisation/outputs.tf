9769b3f6ac10bb6877c519dc77cfc8ec000759d5
Doug <rootprivileges@gmail.com>
2019-10-24 19:00:39 +0100

    Output cloudtrail bucket arn

--- organisation/outputs.tf
+++ organisation/outputs.tf
@@ -18,6 +18,10 @@ output "audit_logs_bucket_id" {
   value = "${aws_s3_bucket.audit_logs.id}"
 }
 
+output "cloudtrail_bucket_arn" {
+  value = "${aws_s3_bucket.cloudtrail.arn}"
+}
+
 output "cloudtrail_bucket_id" {
   value = "${aws_s3_bucket.cloudtrail.id}"
 }
