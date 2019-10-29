c4681d67795cff2945957aa52bef49dd354dfd97
Doug <rootprivileges@gmail.com>
2019-10-29 19:57:23 +0000

    Replace TerragruntInit with Administrator policy on terragrunt.init

--- utility/import-unmanaged-iam-user/main.tf
+++ utility/import-unmanaged-iam-user/main.tf
@@ -1,14 +1,3 @@
-data "aws_caller_identity" "current" {}
-
-data "aws_iam_policy" "policy" {
-  arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/${var.policy_name}"
-}
-
-resource "aws_iam_policy" "policy" {
-  name   = "${var.policy_name}"
-  policy = "${data.aws_iam_policy.policy.policy}"
-}
-
 resource "aws_iam_user" "user" {
   name          = "${var.username}"
   force_destroy = true
@@ -16,5 +5,5 @@ resource "aws_iam_user" "user" {
 
 resource "aws_iam_user_policy_attachment" "attachment" {
   user       = "${aws_iam_user.user.name}"
-  policy_arn = "${aws_iam_policy.policy.arn}"
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
