4d2831b6ff635d7d35d0f6df2e2f675753eb6ad7
Doug <rootprivileges@gmail.com>
2019-11-04 20:00:37 +0000

    Require MFA for IAM actions

--- iam/groups/users/users.tf
+++ iam/groups/users/users.tf
@@ -2,7 +2,13 @@ resource "aws_iam_group" "users" {
   name = "users"
 }
 
-resource "aws_iam_group_policy_attachment" "allow_change_password" {
+resource "aws_iam_policy" "force_mfa" {
+  name   = "Force_MFA"
+  path   = "/"
+  policy = "${data.aws_iam_policy_document.force_mfa.json}"
+}
+
+resource "aws_iam_group_policy_attachment" "assign_force_mfa_policy" {
   group      = "${aws_iam_group.users.name}"
-  policy_arn = "arn:aws:iam::aws:policy/IAMUserChangePassword"
+  policy_arn = "${aws_iam_policy.force_mfa.arn}"
 }
