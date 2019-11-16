d1b49946e1cf58c597f9404903d5a2905e7dcf2a
Doug <rootprivileges@gmail.com>
2019-11-16 17:11:19 +0000

    Clean up some more deprecated interpolations

--- iam/users/finance/finance.tf
+++ iam/users/finance/finance.tf
@@ -1,15 +1,15 @@
 resource "aws_iam_user" "finance_user" {
-  name          = "${var.finance_email}"
+  name          = var.finance_email
   force_destroy = true
 }
 
 resource "aws_iam_user_login_profile" "finance_user" {
-  user    = "${aws_iam_user.finance_user.name}"
+  user    = aws_iam_user.finance_user.name
   pgp_key = "keybase:${var.keybase}"
 }
 
 resource "aws_iam_user_group_membership" "finance_groups" {
-  user = "${aws_iam_user.finance_user.name}"
+  user = aws_iam_user.finance_user.name
 
   groups = [
     "finance",
--- utility/import-unmanaged-iam-user/main.tf
+++ utility/import-unmanaged-iam-user/main.tf
@@ -1,9 +1,9 @@
 resource "aws_iam_user" "user" {
-  name          = "${var.username}"
+  name          = var.username
   force_destroy = true
 }
 
 resource "aws_iam_user_policy_attachment" "attachment" {
-  user       = "${aws_iam_user.user.name}"
+  user       = aws_iam_user.user.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
