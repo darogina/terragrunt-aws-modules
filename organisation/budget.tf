134a28d45aad2cecfc7650c4b3395b12d453aaea
Doug <rootprivileges@gmail.com>
2019-11-27 18:35:51 +0000

    Add Budget alarms for 50% and 100% actual, change forecast to 200%

--- organisation/budget.tf
+++ organisation/budget.tf
@@ -1,7 +1,8 @@
 resource "aws_budgets_budget" "budget" {
+
   name              = "monthly-budget"
   budget_type       = "COST"
-  limit_amount      = var.billing_alarm_threshold
+  limit_amount      = "${var.billing_alarm_threshold}.0"
   limit_unit        = var.billing_alarm_currency
   time_period_start = "2019-01-01_00:00"
   time_unit         = "MONTHLY"
@@ -9,6 +10,22 @@ resource "aws_budgets_budget" "budget" {
   notification {
     comparison_operator        = "GREATER_THAN"
     threshold                  = 50
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "ACTUAL"
+    subscriber_email_addresses = [var.admin_email]
+  }
+
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 100
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "ACTUAL"
+    subscriber_email_addresses = [var.admin_email]
+  }
+
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 200
     threshold_type             = "PERCENTAGE"
     notification_type          = "FORECASTED"
     subscriber_email_addresses = [var.admin_email]
